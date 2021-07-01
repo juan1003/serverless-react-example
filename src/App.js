2cbdc558eb60102e7c033835109e1e701451079d
Juan De Jesus <jdejesus93@gmail.com>
2021-07-01 19:17:55 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  const fetchData = async function() {
+    const response = await (await (fetch('.netlify/functions/helloWorld'))).json();
+    setMessage(response.message);
+  };
+
+  useEffect(() => {
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {message}
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://docs.netlify.com/functions/overview/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Serverless
         </a>
       </header>
     </div>
